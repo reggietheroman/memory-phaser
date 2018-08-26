16c19df42c1399b844ba6dd3dc16323d18d8be17
Reggie <reggietheroman@gmail.com>
2018-08-26 22:41:28 +0800

    Initial commit.

---
+++ config/environment.js
@@ -0,0 +1,14 @@
+/**
+ * Use strict: defines that the Javascript code should be executed in 'strict mode'
+ * 
+ * To use this file: var someVar = require("./../config/environment.js");
+ */
+
+'use strict';
+
+module.exports = function(environment) {
+  let ENV = {
+  };
+
+  return ENV;
+};
